5f864a3c65a74d0af8a12ac48175ba9b22107479
Joachim Metz <joachim.metz@gmail.com>
2015-09-02 20:06:47 +0200

    Changes for parsing update journal.

--- fsntfstools/info_handle.c
+++ fsntfstools/info_handle.c
@@ -3540,24 +3540,18 @@ void info_handle_usn_record_update_reason_flags_fprint(
 		 notify_stream,
 		 "\t\t(USN_REASON_STREAM_CHANGE)\n" );
 	}
-	if( ( update_reason_flags & LIBFUSN_UPDATE_REASON_FLAG_UKNOWN_0x00400000 ) != 0 )
+	if( ( update_reason_flags & LIBFUSN_UPDATE_REASON_TRANSACTED_CHANGE ) != 0 )
 	{
 		fprintf(
 		 notify_stream,
-		 "\t\tUnknown 0x00400000\n" );
+		 "\t\t(USN_REASON_TRANSACTED_CHANGE)\n" );
 	}
+
 	if( ( update_reason_flags & LIBFUSN_UPDATE_REASON_FLAG_CLOSE ) != 0 )
 	{
 		fprintf(
 		 notify_stream,
 		 "\t\t(USN_REASON_CLOSE)\n" );
-	}
-
-	if( ( update_reason_flags & LIBFUSN_UPDATE_REASON_FLAG_UKNOWN_0x80000000 ) != 0 )
-	{
-		fprintf(
-		 notify_stream,
-		 "\t\tUnknown 0x80000000\n" );
 	}
 }
 
