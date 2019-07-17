a4869dd4a2ce52338c4b22f672af92b9f0d66059
Joachim Metz <joachim.metz@gmail.com>
2019-07-17 06:23:16 +0200

    Applied updates

--- tests/fsntfs_test_memory.c
+++ tests/fsntfs_test_memory.c
@@ -35,15 +35,15 @@
 
 #if defined( HAVE_FSNTFS_TEST_MEMORY )
 
-static void *(*fsntfs_test_real_malloc)(size_t)                 = NULL;
-static void *(*fsntfs_test_real_memcpy)(void *, void *, size_t) = NULL;
-static void *(*fsntfs_test_real_memset)(void *, int, size_t)    = NULL;
-static void *(*fsntfs_test_real_realloc)(void *, size_t)        = NULL;
+static void *(*fsntfs_test_real_malloc)(size_t)                       = NULL;
+static void *(*fsntfs_test_real_memcpy)(void *, const void *, size_t) = NULL;
+static void *(*fsntfs_test_real_memset)(void *, int, size_t)          = NULL;
+static void *(*fsntfs_test_real_realloc)(void *, size_t)              = NULL;
 
-int fsntfs_test_malloc_attempts_before_fail                     = -1;
-int fsntfs_test_memcpy_attempts_before_fail                     = -1;
-int fsntfs_test_memset_attempts_before_fail                     = -1;
-int fsntfs_test_realloc_attempts_before_fail                    = -1;
+int fsntfs_test_malloc_attempts_before_fail                           = -1;
+int fsntfs_test_memcpy_attempts_before_fail                           = -1;
+int fsntfs_test_memset_attempts_before_fail                           = -1;
+int fsntfs_test_realloc_attempts_before_fail                          = -1;
 
 /* Custom malloc for testing memory error cases
  * Note this function might fail if compiled with optimation
@@ -82,7 +82,7 @@ void *malloc(
  */
 void *memcpy(
        void *destination,
-       void *source,
+       const void *source,
        size_t size )
 {
 	if( fsntfs_test_real_memcpy == NULL )
