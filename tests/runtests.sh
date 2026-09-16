30af247699e09af029dd82e1e0343bba734f450e
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 07:06:39 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfsntfs/.libs/libfsntfs.1.dylib && test -f ./pyfsntfs/.libs/pyfsntfs.so
+if [ -f "${PWD}/libfsntfs/.libs/libfsntfs.1.dylib" ] && [ -f ./pyfsntfs/.libs/pyfsntfs.so ]
 then
-	install_name_tool -change /usr/local/lib/libfsntfs.1.dylib ${PWD}/libfsntfs/.libs/libfsntfs.1.dylib ./pyfsntfs/.libs/pyfsntfs.so
+    install_name_tool -change /usr/local/lib/libfsntfs.1.dylib "${PWD}/libfsntfs/.libs/libfsntfs.1.dylib" ./pyfsntfs/.libs/pyfsntfs.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
